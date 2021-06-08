59e3d4dfc503828771c22c13c46c1e7815732594
Patricia Novaes <patricia.novaes@gmail.com>
2021-06-07 22:22:13 -0300

    Criando transactions

--- scripts.js
+++ scripts.js
@@ -15,6 +15,29 @@ const Modal = {
     }
 }
 
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+    },
+
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021',
+    },
+
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021',
+    }
+]
+
 const Transaction = {
     incomes() {
         // somar as entradas
